5d1f54e29dc00267108fb698c0671a7f4ccfa5b7
Abdul Soharab <138715814+Abdul-Soharab@users.noreply.github.com>
2024-06-13 10:50:39 +0530

    Add files via upload

---
+++ Array_Lec16.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+using namespace std;
+int main ()
+
+// int Main()
+{
+     cout<<"this is testing"<<endl;
+    cout<<"enter your second  numfcjhvkfiuyfhvhjmn mgbkber:";
+    return 0;
+}
